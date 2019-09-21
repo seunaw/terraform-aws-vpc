8e2388713bdb89c5a4bce2a55c889f8ce0654f70
Seun Wright <seunaw@gmail.com>
2019-09-21 13:13:32 -0700

    Fixes

--- main.tf
+++ main.tf
@@ -1043,8 +1043,10 @@ resource "aws_nat_gateway" "this" {
     var.single_nat_gateway ? 0 : count.index,
   )
   # @Swright
-  subnet_id = !var.subnet_with_names ? element(aws_subnet.public.*.id,var.single_nat_gateway ? 0 : count.index) : 
-    element(aws_subnet.public_with_names.*.id,var.single_nat_gateway ? 0 : count.index)
+  # subnet_id = !var.subnet_with_names ? element(aws_subnet.public.*.id,var.single_nat_gateway ? 0 : count.index) : 
+  #   element(aws_subnet.public_with_names.*.id,var.single_nat_gateway ? 0 : count.index)
+
+  subnet_id = !var.subnet_with_names ? 1 : 0
   #element(aws_subnet.public.*.id,var.single_nat_gateway ? 0 : count.index) : 
 
   #element( aws_subnet.public_subnets_with_names.*.id,var.single_nat_gateway ? 0 : count.index) : 0 
