65e86728694312dd399041bbca2f6a05dd0896ae
Seun Wright <seunaw@gmail.com>
2019-09-24 21:13:30 -0700

    Fixes

--- main.tf
+++ main.tf
@@ -90,7 +90,7 @@ resource "aws_vpc_dhcp_options_association" "this" {
 # Internet Gateway
 ###################
 resource "aws_internet_gateway" "this" {
-  count = var.create_vpc && (length(var.public_subnets) > 0 || length(var.public_subnets_with_names) > 0) ? 1 : 0
+  count = var.create_vpc && (length(var.public_subnets) > 0 || length(var.public_subnets_with_names) > 0 || length(var.outbound_subnets) > 0 || length(var.outbound_subnets_with_names) > 0 ) ? 1 : 0
 
   vpc_id = local.vpc_id
 
